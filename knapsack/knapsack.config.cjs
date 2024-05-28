22ba3e82c1f45c5d8806aaa7653a71d534d3765f
Brittany Smart <30271981+brittanysmart@users.noreply.github.com>
2024-05-28 17:03:18 -0500

    Install Knapsack v4

---
+++ knapsack/knapsack.config.cjs
@@ -0,0 +1,25 @@
+const { KnapsackReactRenderer } = require('@knapsack/renderer-react');
+const { configureKnapsack } = require('@knapsack/app');
+const { join } = require('path');
+const { version } = require('./../package.json');
+
+module.exports = configureKnapsack({
+  data: join(__dirname, './data'),
+  dist: join(__dirname, './dist'),
+  public: join(__dirname, './public'),
+  version,
+  templateRenderers: [
+    new KnapsackReactRenderer({
+      webpackConfig: {
+        module: {
+          rules: [],
+        },
+      },
+    }),
+  ],
+  plugins: [],
+  cloud: {
+    siteId: 'public-demo',
+    repoRoot: join(__dirname, '..'),
+  },
+});
