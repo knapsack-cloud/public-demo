c54462936e2e9bea2316b48c4775b780ac478325
Josh Mabry <mabry1985@gmail.com>
2024-06-11 11:17:06 -0700

    update config

--- knapsack/knapsack.config.cjs
+++ knapsack/knapsack.config.cjs
@@ -1,25 +1,17 @@
-const { KnapsackReactRenderer } = require('@knapsack/renderer-react');
-const { configureKnapsack } = require('@knapsack/app');
-const { join } = require('path');
-const { version } = require('./../package.json');
+const { KnapsackReactRenderer } = require("@knapsack/renderer-react");
+const { configureKnapsack } = require("@knapsack/app");
+const { join } = require("path");
+const { version } = require("./../package.json");
 
 module.exports = configureKnapsack({
-  data: join(__dirname, './data'),
-  dist: join(__dirname, './dist'),
-  public: join(__dirname, './public'),
+  data: join(__dirname, "./data"),
+  dist: join(__dirname, "./dist"),
+  public: join(__dirname, "./public"),
   version,
-  templateRenderers: [
-    new KnapsackReactRenderer({
-      webpackConfig: {
-        module: {
-          rules: [],
-        },
-      },
-    }),
-  ],
+  templateRenderers: [new KnapsackReactRenderer()],
   plugins: [],
   cloud: {
-    siteId: 'public-demo',
-    repoRoot: join(__dirname, '..'),
+    siteId: "public-demo",
+    repoRoot: join(__dirname, ".."),
   },
 });
